d5b7a631853af2aeb6e9ee278305a0aca0c905de
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-10-29 19:49:27 +0100

    Add extract_message function

---
+++ R/extract_message.R
@@ -0,0 +1,23 @@
+#' Returns a RProtoBuf message as list structure
+#'
+#' [RProtoBuf::as.list.Message()] does not coerce the message fields. [extract_message()] inspects the whole tree of message fields recursively and returns the message as tree structure of lists and atomic leafs.
+#'
+#' @param x A RProtoBuf Message, list or atomic
+#' @return A list if x is a Message or recursive, or an atomic if x is atomic
+#' @importFrom lubridate as_datetime
+#' @export
+extract_message <- function(x) {
+  if (is.atomic(x)) {
+    return(x)
+  }
+  if (is.recursive(x)) {
+    return(mapply(x, FUN = extract_message))
+  }
+  if (class(x) == "Message") {
+    if (name(descriptor(x)) == "Timestamp") {
+      as_datetime(x$seconds)
+    } else {
+      mapply(as.list(x), FUN = extract_message)
+    }
+  }
+}
