84d2b1c0ab9ae64a7b767028ca37dc1a742b8c33
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-10-30 05:59:54 +0100

    Add docs and fix check error

--- R/create_simulation.R
+++ R/create_simulation.R
@@ -1,10 +1,22 @@
 #' Creates a simulation config from data
 #'
+#' A convenience helper for creating simulations with multiple conditions.
+#'
+#' The params list can contain the following variables:
+#' \describe{
+#'   \item{start_letters <chr>}{A character vector of four-letter combinations. If omitted, the dataset Begriffix_Startbuchstaben from this package is used.}
+#'   \item{trials <int>}{Indicates how many games should be played per condition.}
+#'   \item{starter_wordlist_path <chr>}{The file which contains the serialized word list for the start player.}
+#'   \item{starter_wordlist_(prefix|suffix|sample) <int>}{Indicates how many words should be taken from the starter's word list. If omitted, the complete list is used. You can supply exactly one of the three variants.}
+#'   \item{opponent_wordlist_path <chr>}{The file which contains the serialized word list for the opponent player. If omitted, starter_wordlist_path is inherited.}
+#'   \item{opponent_wordlist_(prefix|suffix|sample) <int>}{Indicates how many words should be taken from the opponent's word list. If omitted, starter_wordlist_... is used. You can supply exactly one of the three variants.}
+#' }
+#'
 #' @param title A character scalar, a short title for the simulation.
 #' @param supplement A character scalar, more optional explanations.
-#' @param params A list of vectors.
+#' @param params A list of vectors. Each combination of these vectors becomes a simulation condition.
 #' @return A RProtoBuf Message of type strategic_games.Simulation.
-#' @importFrom purrr cross map map_chr
+#' @importFrom purrr cross map map_chr %||%
 #' @import RProtoBuf
 #' @export
 create_simulation <- function(title, supplement = NULL, params) {
@@ -25,11 +37,22 @@ create_simulation <- function(title, supplement = NULL, params) {
 
 create_condition <- function(x) {
   condition <- new(P("strategic_games.Simulation.Condition"),
-    start_letters = params$start_letters,
-    trials = params$trials,
+    start_letters = x$start_letters,
+    trials = x$trials,
     starter = new(P("strategic_games.Simulation.Condition.Player"),
       vocabulary = new(P("strategic_games.Vocabulary"),
-        path = params$starter_wordlist_path
+        path = x$starter_wordlist_path,
+        prefix = x$starter_wordlist_prefix %||% 0,
+        suffix = x$starter_wordlist_suffix %||% 0,
+        sample = x$starter_wordlist_sample %||% 0
+      )
+    ),
+    opponent = new(P("strategic_games.Simulation.Condition.Player"),
+      vocabulary = new(P("strategic_games.Vocabulary"),
+        path = x$opponent_wordlist_path %||% x$starter_wordlist_path,
+        prefix = x$opponent_wordlist_prefix %||% 0,
+        suffix = x$opponent_wordlist_suffix %||% 0,
+        sample = x$opponent_wordlist_sample %||% 0
       )
       )
     )
