e67ca361716b935b8d40ec540d46eff2987b3a9e
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-10-31 06:13:07 +0100

    Convert Begriffix_Startbuchstaben to lower case

--- data-raw/begriffix_startbuchstaben.R
+++ data-raw/begriffix_startbuchstaben.R
@@ -5,5 +5,7 @@ Begriffix_Startbuchstaben <- readr::read_delim(
   col_names = c("44", "45", "54", "55")
 )
 usethis::use_data(Begriffix_Startbuchstaben, overwrite=T)
-start_letters <- purrr::pmap_chr(Begriffix_Startbuchstaben, paste0)
-usethis::use_data(start_letters, internal = T)
+start_letters <- stringr::str_to_lower(
+  purrr::pmap_chr(Begriffix_Startbuchstaben, paste0)
+)
+usethis::use_data(start_letters, internal = T, overwrite = T)
