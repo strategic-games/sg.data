266707cf5d954e63300311a8cbe2d57d50bae101
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-11-15 13:16:17 +0100

    Update docs

--- R/simulations.R
+++ R/simulations.R
@@ -1,6 +1,7 @@
 #' Begriffix simulations with various vocabulary sizes
 #'
 #' @usage data(simulation_vocabulary)
+#' @format A data frame with 4750 rows and 16 variables where each row represents a game.
 #' @source generated with [Hangman](https://github.com/strategic-games/hangman)
 #' @family simulation data
 "simulation_vocabulary"
