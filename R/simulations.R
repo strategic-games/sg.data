754dfc956e5ccaae99ec7e7182673e8b074cc847
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-01-30 21:57:45 +0100

    Update docs

--- R/simulations.R
+++ R/simulations.R
@@ -19,3 +19,10 @@
 #' @source generated with [Hangman](https://github.com/strategic-games/hangman)
 #' @family simulation data
 "simulation_vocabulary_normal"
+
+#' Full combination of simulation input variables
+#'
+#' @format Hangman simulation with 10.000 games.
+#' @source generated with [Hangman](https://github.com/strategic-games/hangman)
+#' @family simulation data
+"simulation_thesis"
