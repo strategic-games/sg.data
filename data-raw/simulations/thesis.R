529693e8aa5d5b9554a0c1f618590da33b3e3c6d
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-03-04 13:44:34 +0100

    Update thesis script

--- data-raw/simulations/thesis.R
+++ data-raw/simulations/thesis.R
@@ -1,86 +1,60 @@
 # packages
 library(tidyverse)
-library(magrittr)
+library(sg.data)
 
 set.seed(3825)
-wordlist <- serialize_wordlist()
-vocabulary <- new(strategic_games.Vocabulary,
-  path = wordlist
-)
 
-# Unabhängige Variablen
+# Spielermerkmale
 # Wortschatzgrößen
-vocabulary_size_stddev <- function() {
-  m <- 50000
-  s <- 15000
-  seq(m-2*s, m+2*s, by = s)
-}
-vocabulary_size <- vocabulary_size_stddev()
+vocabulary_size <- -2:2*15000+50000
 # Spielerstrategien
 strategies <- c("random", "min_places", "availability")
-# Spielfeldgrößen
-board_size <- as.integer(c(8, 10))
-# Mindestwortlängen
-word_min_lengths <- list(
-  strategic_games.Simulation.Condition.WordMinLength$new(
-    restricted = 5, liberal = 4
+wordlist <- serialize_wordlist()
+grid <- list(
+  start_letters_origin = c("random", "human"),
+  board_size = as.integer(c(8, 10)),
+  word_min_length = list(list(first=5, other=4), list(first=4, other=3)),
+  direction_restrictions = list(
+    list(first="starter", other="none"), list(first="fixed", other="none")
   ),
-  strategic_games.Simulation.Condition.WordMinLength$new(
-    restricted = 4, liberal = 3
-  )
-)
-# Startbuchstaben
-start_letters_origin <- c("human", "random")
-sl_random <- function() {
-  DeReChar %>%
-  slice(-31) %$%
-  sample(G, size = 4, prob = RF) %>%
-  paste0(collapse = "")
-}
-sl <- function(x) {
-  switch(x,
-    "human" = sample(start_letters, size = 1, replace = T),
-    "random" = sl_random()
-  )
-}
-
-# Kombinieren
-var_list <- list(
-  start_letters_origin = start_letters_origin,
   starter_prefix = vocabulary_size,
   starter_strategy = strategies,
   opponent_prefix = vocabulary_size,
-  opponent_strategy = strategies,
-  board_size = board_size,
-  word_min_length = word_min_lengths
-)
-start_letters_rep <- var_list %>%
-cross_df() %$%
-rep(start_letters_origin, each = 10)
+  opponent_strategy = strategies
+) %>%
+cross()
 
-conditions <- var_list %>%
-cross() %>%
-map(function(x) {
+conditions <- grid %>%
+lapply(function(x) {
   new(strategic_games.Simulation.Condition,
-    start_letters = sl(x$start_letters_origin),
+    start_letters = generate_start_letters(x[["start_letters_origin"]]),
+    board_size = x[["board_size"]],
+    word_min_length = do.call(
+      strategic_games.Simulation.Condition.WordMinLength$new,
+      x[["word_min_length"]]
+    ),
+    direction_restrictions = do.call(
+      strategic_games.Simulation.Condition.DirectionRestrictions$new,
+      x[["direction_restrictions"]]
+    ),
     starter = new(strategic_games.Simulation.Condition.Player,
       vocabulary = new(strategic_games.Vocabulary,
         path = wordlist,
-        prefix = x$starter_prefix
+        prefix = x[["starter_prefix"]]
       ),
-      begriffixStrategy = x$starter_strategy
+      begriffixStrategy = x[["starter_strategy"]]
     ),
     opponent = new(strategic_games.Simulation.Condition.Player,
       vocabulary = new(strategic_games.Vocabulary,
         path = wordlist,
-        prefix = x$opponent_prefix
+        prefix = x[["opponent_prefix"]]
       ),
-      begriffixStrategy = x$opponent_strategy
+      begriffixStrategy = x[["opponent_strategy"]]
     ),
     trials = 10,
-    vocabulary = vocabulary,
-    board_size = x$board_size,
-    word_min_length = x$word_min_length
+    vocabulary = new(strategic_games.Vocabulary,
+      path = wordlist
+    )
   )
 })
 
@@ -92,10 +66,9 @@ config <- new(strategic_games.Simulation,
   conditions = conditions
 )
 
-simulation_thesis <- run_simulation(config)
-raw <- simulation_thesis
+raw <- run_simulation(config)
 
-simulation_thesis %<>%
+simulation_thesis <- raw %>%
 extract_message() %>%
 simulation2df() %>%
 mutate_at(
@@ -107,6 +80,11 @@ rename(
   opponent_wordlist_name = opponent_path,
   vocabulary_name = vocabulary_path
 ) %>%
-add_column(start_letters_origin = start_letters_rep, .after = 3)
+add_column(
+  start_letters_origin = rep(
+    map_chr(grid, "start_letters_origin"),
+    each = 10),
+  .after = 3
+)
 
 usethis::use_data(simulation_thesis, compress = "xz", overwrite=T)
