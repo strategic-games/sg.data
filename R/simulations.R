3f27ef490b11a6bd78e49395118958937d761588
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-06-26 18:22:07 +0200

    Rename simulation dataset

--- R/simulations.R
+++ R/simulations.R
@@ -3,4 +3,4 @@
 #' @format Hangman simulation with 10.000 games.
 #' @source generated with [Hangman](https://github.com/strategic-games/hangman)
 #' @family simulation data
-"simulation_thesis_0.6"
+"simulation_thesis"
