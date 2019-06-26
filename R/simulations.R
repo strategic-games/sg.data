d84160fb982f178d3a530ac4f1a58b791aa35a7c
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-06-26 17:54:46 +0200

    Slim down code

--- R/simulations.R
+++ R/simulations.R
@@ -1,28 +1,6 @@
-#' Begriffix simulations with various vocabulary sizes
-#'
-#' @usage data(simulation_vocabulary)
-#' @format Hangman simulation with 4.750 games.
-#' @source generated with [Hangman](https://github.com/strategic-games/hangman)
-#' @family simulation data
-"simulation_vocabulary"
-
-#' Compare players with long and short move selection strategy
-#'
-#' @format Hangman simulation with 380 games.
-#' @source generated with [Hangman](https://github.com/strategic-games/hangman)
-#' @family simulation data
-"simulation_strategy"
-
-#' Compare normally distributed vocabulary sizes (m = 50.000, s = 15.000)
-#'
-#' @format Hangman simulation with 10.000 games.
-#' @source generated with [Hangman](https://github.com/strategic-games/hangman)
-#' @family simulation data
-"simulation_vocabulary_normal"
-
 #' Full combination of simulation input variables
 #'
 #' @format Hangman simulation with 10.000 games.
 #' @source generated with [Hangman](https://github.com/strategic-games/hangman)
 #' @family simulation data
-"simulation_thesis"
+"simulation_thesis_0.6"
