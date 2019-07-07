8420f109dd09fcf4e26f89492ac6fe978d9ff712
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-07-07 21:46:07 +0200

    Document info list

---
+++ R/thesis_info.R
@@ -0,0 +1,9 @@
+#' Metadata about the thesis data
+#' @usage data(thesis_info, package = "sg.data")
+#' @format A named list
+#' \describe{
+#'   \item{title, supplement}{Textual name and description}
+#'   \item{date}{When the simulation was generated}
+#'   \item{version}{The used Hangman version}
+#' }
+"thesis_info"
