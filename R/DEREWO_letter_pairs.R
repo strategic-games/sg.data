72898d609b5a5839ef0a910afcccf4adc1e96fbc
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-08-06 18:38:59 +0200

    Add frequency table of letter pairs

---
+++ R/DEREWO_letter_pairs.R
@@ -0,0 +1,6 @@
+#' Letter pairs found in DEREWO_Wortformen with their frequencies
+#'
+#' @usage data(DEREWO_letter_pairs)
+#' @format A 1D table with 825 entries
+#' @family auxiliary data
+"DEREWO_letter_pairs"
---
+++ data-raw/derewo_letter_pairs.R
@@ -0,0 +1,18 @@
+library("pipeR")
+library(purrr)
+library(stringr)
+data(DEREWO_Wortformen)
+
+DEREWO_letter_pairs <- DEREWO_Wortformen$word %>>%
+map(function(x) {
+  n <- str_length(x) - 1
+  if (n < 1) return(NA)
+  if (n == 1) return(str_to_lower(x))
+  start <- 1:n
+  end <- start + 1
+  str_sub(str_to_lower(x), start, end)
+}) %>>%
+flatten_chr() %>>%
+table()
+
+usethis::use_data(DEREWO_letter_pairs)
