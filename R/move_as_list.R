2a27e8d575a84a747faa89f9c1e9de498275611a
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-08-09 18:42:18 +0200

    Postprocess trials and moves after converting to df

--- R/move_as_list.R
+++ R/move_as_list.R
@@ -1,6 +1,11 @@
-move_as_list <- function(m) {
+move_as_list <- function(m, direction_levels, direction_labels) {
   stopifnot(requireNamespace("purrr", quietly = TRUE))
   hits <- purrr::map_dfr(m$hits, hit_as_list)
+  hits <- purrr::modify_if(hits, is.double, as.integer)
+  hits <- purrr::modify_at(
+    hits, "direction",
+    factor, levels = direction_levels, labels = direction_labels
+  )
   list(
     row = m$place$row,
     column = m$place$column,
--- R/trial_as_list.R
+++ R/trial_as_list.R
@@ -1,6 +1,13 @@
-trial_as_list <- function(t) {
+trial_as_list <- function(t, direction_levels, direction_labels) {
   stopifnot(requireNamespace("purrr", quietly = TRUE))
-  moves <- purrr::map_dfr(t$moves, move_as_list, .id = "move")
+  stopifnot(requireNamespace("tibble", quietly = TRUE))
+  moves <- purrr::map_dfr(t$moves, move_as_list, direction_levels = direction_levels, direction_labels = direction_labels)
+  moves <- purrr::modify_if(moves, is.double, as.integer)
+  moves <- tibble::rowid_to_column(moves, var = "move_id")
+  moves <- purrr::modify_at(
+    moves, "direction",
+    factor, levels = direction_levels, labels = direction_labels
+  )
   list(
     condition = t$condition,
     trial = t$trial,
