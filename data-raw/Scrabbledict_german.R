aaf8551daf52262f03a3515bb06ac1ab261e07ff
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-08-10 18:08:30 +0200

    Fix wrong NA in scrabbledict_german

--- data-raw/Scrabbledict_german.R
+++ data-raw/Scrabbledict_german.R
@@ -1,7 +1,8 @@
-Scrabbledict_german <- readr::read_delim(
-  file = system.file("data-raw", "Scrabbledict_german.txt", package="sg.simulate"),
+Scrabbledict_german <- vroom::vroom(
+  file = system.file("data-raw", "Scrabbledict_german.txt", package="sg.data"),
   delim = " ",
   col_names = "word",
-  col_types = "c"
+  col_types = "c",
+  na = character()
 )
 usethis::use_data(Scrabbledict_german, overwrite = T, compress="xz")
